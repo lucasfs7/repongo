19e752c7b267bb257943414103703a868df47581
lucasfs7 <me@lucasfsouza.com.br>
2022-03-03 00:06:34 -0300

    remove console.log

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Game from './components/Game'
 import { WebsocketProvider } from './contexts/websocket'
 
 const serverUrl = `ws://${process.env.REACT_APP_SERVER_URL}`
-console.log(serverUrl)
 
 export default function App() {
   return (
